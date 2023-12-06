75ee521963d1027e16aa4f157e7326bb87e0d705
Steven He <hez2010@outlook.com>
2023-12-07 02:56:59 +0900

    Add WinRT out-of-proc server demo

---
+++ WinRTServer/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+
+namespace WinRTServer
+{
+    public sealed class TestClass
+    {
+        public IAsyncOperation<string> HelloAsync()
+        {
+            Console.WriteLine("HelloAsync is being called.");
+            return AsyncInfo.Run(async token =>
+            {
+                await Task.Delay(1000, token);
+                return "Hello from server after 1 sec.";
+            });
+        }
+    }
+}
---
+++ WinRTServerTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WinRTServer;
+
+
+Console.WriteLine("Activating the class.");
+
+var obj = new TestClass();
+
+Console.WriteLine("Calling into the class.");
+
+Console.WriteLine(await obj.HelloAsync());
+
+Console.ReadLine();
